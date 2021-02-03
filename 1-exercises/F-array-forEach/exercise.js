7bc58f5c25c0568c4cbb1b147a7ca6c219555d60
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-02-03 22:57:44 +0200

    F-array-forEach-exercise.js

--- 1-exercises/F-array-forEach/exercise.js
+++ 1-exercises/F-array-forEach/exercise.js
@@ -8,9 +8,12 @@
 */
 
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+for (let i = 0; i < 101; i++) {
+  arr[i] = i;
+}
 
 arr.forEach(function (x) {
-  if (x % 3 === 0 && x % 5 === 0) {
+  if (x % 15 === 0) {
     console.log("FizzBuzz");
   } else if (x % 5 === 0) {
     console.log("Fizz");
